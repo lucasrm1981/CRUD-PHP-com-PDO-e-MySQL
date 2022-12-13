f2e26798b51ed82b0c1957b0c95110f612a94683
Lucas Rodrigues Monteiro <47841382+lucasrm1981@users.noreply.github.com>
2022-12-12 21:16:13 -0300

    Update consult.php

--- consult.php
+++ consult.php
@@ -1,14 +1,28 @@
 <?php
-    // PHP DATA OBJECT - PHP Orinetado a Objetos
+    /* PHP DATA OBJECT - PHP Orinetado a Objetos*/
     $username = 'root'; /* Nome do usuário do Banco*/
     $password = ''; /* Senha do usuário do Banco*/
+    
+
+
+$id_user = $_POST['id_user'];
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+$senha = sha1($login);
+
+try {
     $pdo = new PDO('mysql:host=localhost;dbname=system', $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);/* Irá exibir as exceções erros ao objeto pdo */
 
-    // Faz a consuta nas tabelas desejadas
-$consulta = $pdo->query("SELECT * FROM users;");
+    /* Prepara o objeto statement e faz a execução da query*/
+  $stmt = $pdo->prepare('UPDATE users SET login = :login, senha = :senha WHERE id_user = :id_user');
+  $stmt->execute(array(
+    ':id_user' => $id_user,
+    ':login' => $login,
+    ':senha' => $senha,
+  ));
 
-
-while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-    echo "ID: {$linha['id_user']} - Login: {$linha['login']} <a href='form_update.php?id_user={$linha['id_user']}'>Atualizar</a>---<a href='delete.php?id_user={$linha['id_user']}'>Deletar</a><br /> ";
+  echo 'Linhas da Tabela Afetadas '.$stmt->rowCount(); /* Linhas afetadas */
+} catch(PDOException $e) {
+  echo 'Error: ' . $e->getMessage();
 }
