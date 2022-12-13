a71f8b25a54e379593673f393132b241aaf66388
Lucas Rodrigues Monteiro <47841382+lucasrm1981@users.noreply.github.com>
2022-12-12 21:17:22 -0300

    Update update.php

--- update.php
+++ update.php
@@ -1,22 +1,25 @@
 <?php
-    // PHP DATA OBJECT - PHP Orinetado a Objetos
+    /* PHP DATA OBJECT - PHP Orinetado a Objetos*/
     $username = 'root'; /* Nome do usuário do Banco*/
     $password = ''; /* Senha do usuário do Banco*/
     
 
 
-$id_user = 4;
-$login = "Novo Admin";
-$login = sha1($login);
+$id_user = $_POST['id_user'];
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+$senha = sha1($login);
 
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=system', $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);/* Irá exibir as exceções erros ao objeto pdo */
 
-  $stmt = $pdo->prepare('UPDATE users SET login = :login WHERE id_user = :id_user');
+    /* Prepara o objeto statement e faz a execução da query*/
+  $stmt = $pdo->prepare('UPDATE users SET login = :login, senha = :senha WHERE id_user = :id_user');
   $stmt->execute(array(
-    ':id_user'   => $id_user,
-    ':login' => $login
+    ':id_user' => $id_user,
+    ':login' => $login,
+    ':senha' => $senha,
   ));
 
   echo 'Linhas da Tabela Afetadas '.$stmt->rowCount(); /* Linhas afetadas */
